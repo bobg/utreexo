a9c314a7781ad124248939ce1ac257e0135378b3
bobg <bobg@emphatic.com>
2019-04-16 07:48:00 -0400

    Revert "ensure unaltered proof" change, which was wrong, and update doc.

--- proof.go
+++ proof.go
@@ -23,30 +23,25 @@ type (
 var ErrInvalid = errors.New("invalid proof")
 
 // Update updates the proof of inclusion for a value after the Utreexo has been updated.
-// In case of error, the proof is unaltered.
-func (p *Proof) Update(u Update) (err error) {
+// In case of error
+// (i.e., the proof is invalid with respect to the Utreexo in u),
+// the proof may be incompletely updated and should be discarded.
+func (p *Proof) Update(u Update) error {
 	h := p.Leaf
-	steps := p.Steps
 
-	defer func() {
-		if err == nil {
-			p.Steps = steps
-		}
-	}()
-
-	for i := 0; i <= len(steps); i++ {
+	for i := 0; i <= len(p.Steps); i++ {
 		if len(u.u.roots) > i && u.u.roots[i] != nil && *u.u.roots[i] == h {
-			steps = steps[:i]
+			p.Steps = p.Steps[:i]
 			return nil
 		}
 		var step ProofStep
 		if s, ok := u.updated[h]; ok {
 			step = s
-			steps = append(steps[:i], step)
-		} else if i == len(steps) {
+			p.Steps = append(p.Steps[:i], step)
+		} else if i == len(p.Steps) {
 			break
 		} else {
-			step = steps[i]
+			step = p.Steps[i]
 		}
 		if step.Left {
 			h = u.u.hasher(step.H, h)
